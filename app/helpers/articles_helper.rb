c4be50ba5aae12236d003f0ca587bf9afa4a6512
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-06-13 01:17:51 -0700

    Render markdown as HTML for article text param.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,3 +1,14 @@
+# Helper methods for article views
 module ArticlesHelper
+  module_function
+
   LABELS = %i[title author description].freeze
+
+  # Parser to convert markdown into HTML
+  MARKDOWN_PARSER = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+
+  # Render markdown and return HTML
+  def render_html!(text)
+    MARKDOWN_PARSER.render(text)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,11 +4,4 @@
 class Article < ApplicationRecord
   validates :title,  presence: true
   validates :author, presence: true
-
-  @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-
-  # Render markdown and return HTML
-  def render!(text)
-    @markdown.render(text)
-  end
 end
