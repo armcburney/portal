c02ec9f0feff447d9bab15159de2077fd180b440
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-06-14 23:27:15 -0700

    Update admin settings in browser.

--- app/assets/javascripts/admin.js.ts
+++ app/assets/javascripts/admin.js.ts
@@ -20,11 +20,22 @@ class AdminSettings {
         });
     }
 
-    private getAdminSettings(callback: any) :any {
+    setColour(id: String, value: String) {
+        $(`#${id}`).css({ 'color': value });
+    }
+
+    private getAdminSettings(callback: any): any {
         return $.get("admin_settings/", (response) => callback(response));
     }
 }
 
 $(() => {
     var admin = new AdminSettings();
+
+    $('input[type="text"]').keypress((event: Event) => {
+        let targetId: string = $(event.target)[0].id.substring(0, 2);
+        let targetVal: string = $(event.target).val();
+        admin.setColour(targetId, targetVal);
+    });
 });
+
