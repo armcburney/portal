2d14c56993dae1f8bcee44d5b1e5afaf135acbcf
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-06-17 21:46:20 -0700

    Added friendly id.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@
 # Article.rb model
 #
 class Article < ApplicationRecord
+  extend FriendlyId
+
   validates :title,  presence: true
   validates :author, presence: true
+
+  friendly_id :title, use: :slugged
 end
